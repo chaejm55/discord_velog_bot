cd8a44fad7bca4fd46f3edbf366e58f4124c199b
chaejm55 <chaejm55@gmail.com>
2021-03-06 05:15:05 +0900

    1. playing with bot

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord
+import os
+import random
 from discord.ext import commands
 
 token = '토큰 붙여넣기'
@@ -15,5 +17,33 @@ async def on_ready():  # 봇 준비 시 1회 동작하는 부분
 @bot.command()  # 봇 명령어
 async def hello(ctx):  # !hello라고 사용자가 입력하면
     await ctx.send("Hello world")  # 봇이 Hello world!라고 대답함
+
+
+@bot.command()
+async def dice(ctx):
+    randnum = random.randint(1, 6)
+    await ctx.send(f'주사위 결과는 {randnum} 입니다.')
+
+
+@bot.command()
+async def mining(ctx):
+    minerals = ['다이아몬드', '루비', '에메랄드', '자수정', '철', '석탄']
+    weights = [1, 3, 6, 15, 25, 50]
+    results = random.choices(minerals, weights=weights, k=5)
+    await ctx.send(', '.join(results) + ' 광물들을 획득하였습니다.')
+
+
+@bot.command()
+async def game(ctx, user: str):
+    rps_table = ['가위', '바위', '보']
+    bot = random.choice(rps_table)
+    result = rps_table.index(user) - rps_table.index(bot)
+    if result == 0:
+        await ctx.send(f'{user} vs {bot}  비겼습니다.')
+    elif result == 1 or result == -2:
+        await ctx.send(f'{user} vs {bot}  유저가 이겼습니다.')
+    else:
+        await ctx.send(f'{user} vs {bot}  봇이 이겼습니다.')
+
 
 bot.run(token)
