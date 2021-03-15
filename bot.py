bd07a6059da7b74eabfa1649f8f8657a4b5838a6
chaejm55 <chaejm55@gmail.com>
2021-03-15 22:32:24 +0900

    4. 간단한 크롤링 해보기

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import random
 from discord.ext import commands
+from bs4 import BeautifulSoup
+import requests
 
 token = '토큰 붙여넣기'
 bot = commands.Bot(command_prefix='!')  # 봇의 접두사 설정
@@ -63,5 +65,34 @@ async def embed(ctx):
 @bot.command(usage='test usage', description='test description', help='test help')
 async def test(ctx):
     await ctx.send("test")
+
+
+@bot.command(aliases=['코로나'])  # !코로나 입력 시에도 실행 가능
+async def crawl(ctx):
+    url = "http://ncov.mohw.go.kr/"
+    response = requests.get(url)
+    response_code = int(response.status_code)  # 응답 코드 받기
+
+
+    if response_code == 200:  # 정상 작동(코드 200 반환) 시
+        soup = BeautifulSoup(response.content, 'lxml')
+    else:  # 오류 발생
+        await ctx.send("웹 페이지 오류입니다.")
+
+    # element 찾기
+
+    # soup.find ()로 <div class="liveNum_today_new"> 에서 확진자 수 데이터가 들어 있는 <span class="data"> 리스트 가져오기
+    today = soup.find("div", {"class": "liveNum_today_new"}).findAll("span", {"class": "data"})
+    today_domestic = int(today[0].text)  # 리스트 첫 번째 요소 (국내발생)
+    # today_domestic = int(soup.select_one("body > div > div.mainlive_container > div.container > div > div.liveboard_layout > div.liveNumOuter > div.liveNum_today_new > div > ul > li:nth-child(1) > span.data").text)
+    today_overseas = int(today[1].text) # 리스트 두 번째 요소 (해외유입)
+    accumulate_confirmed = soup.find("div", {"class": "liveNum"}).find("span", {"class": "num"}).text[4:]  # 앞에 (누적) 글자 자르기
+
+    embed = discord.Embed(title="국내 코로나 확진자 수 현황", description="http://ncov.mohw.go.kr/ 의 정보를 가져옵니다.", color=0x005666)
+    embed.add_field(name="일일 확진자",
+                value=f"총: {today_domestic + today_overseas}, 국내: {today_domestic}, 해외유입: {today_overseas}",
+                inline=False)
+    embed.add_field(name="누적 확진자", value=f"{accumulate_confirmed}명", inline=False)
+    await ctx.send(embed=embed)
 
 bot.run(token)
