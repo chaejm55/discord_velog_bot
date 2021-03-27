33413b45c13d5ef4d6d8f15620a4b6f5d5e1bff4
chaejm55 <chaejm55@gmail.com>
2021-03-27 23:39:33 +0900

    7. 유저 관리2

--- bot.py
+++ bot.py
@@ -128,4 +128,25 @@ async def ban_user(ctx, nickname: discord.Member):
     await nickname.ban()
     await ctx.send(f"{nickname} 님이 차단되었습니다.")
 
+
+@bot.command(aliases=['해제'])
+async def unban_user(ctx, nickname: str):
+    ban_entry = await ctx.guild.bans()
+    for users in ban_entry:
+        if nickname == users.user.name:
+            forgive_user = users.user
+            await ctx.guild.unban(forgive_user)
+            return await ctx.send(f"{nickname} 님이 차단 해제되었습니다.")
+    return await ctx.send(f"{nickname} 님은 차단 목록에 없습니다.")
+
+
+@bot.command(aliases=['역할부여'])
+async def role_user(ctx, nickname: discord.Member, role_name):
+    roles = ctx.guild.roles
+    for role in roles:
+        if role_name == role.name:
+            await nickname.add_roles(role)
+            return await ctx.send(f"{nickname} 님에게 {role_name} 역할이 부여 되었습니다.")
+    return await ctx.send(f"{role_name} 역할이 존재하지 않습니다.")
+
 bot.run(token)
