ba2b6fcddc9b100c357c1104e954322733b43946
chaejm55 <chaejm55@gmail.com>
2021-09-16 10:49:22 +0900

    crawl 커맨드 오류 수정

--- Cogs/Util.py
+++ Cogs/Util.py
@@ -19,21 +19,21 @@ class Util(commands.Cog):
         if response_code == 200:  # 정상 작동(코드 200 반환) 시
             soup = BeautifulSoup(response.content, 'lxml')
         else:  # 오류 발생
-            await ctx.send("웹 페이지 오류입니다.")
+            return await ctx.send("웹 페이지 오류입니다.")
 
         # element 찾기
 
         # soup.find ()로 <div class="liveNum_today_new"> 에서 확진자 수 데이터가 들어 있는 <span class="data"> 리스트 가져오기
         today = soup.find("div", {"class": "liveNum_today_new"}).findAll("span", {"class": "data"})
-        today_domestic = int(today[0].text)  # 리스트 첫 번째 요소 (국내발생)
-        # today_domestic = int(soup.select_one("body > div > div.mainlive_container > div.container > div > div.liveboard_layout > div.liveNumOuter > div.liveNum_today_new > div > ul > li:nth-child(1) > span.data").text)
-        today_overseas = int(today[1].text)  # 리스트 두 번째 요소 (해외유입)
+        today_domestic = today[0].text  # 리스트 첫 번째 요소 (국내발생), int() 사용 대신 string으로 바로 넣음
+        today_overseas = today[1].text  # 리스트 두 번째 요소 (해외유입), int() 사용 대신 string으로 바로 넣음
+        today_total = format((int(today_domestic.replace(',', '')) + int(today_overseas.replace(',', ''))), ',')  # 일일 총 확진자 수를 따로 계산
         accumulate_confirmed = soup.find("div", {"class": "liveNum"}).find("span", {"class": "num"}).text[
                                4:]  # 앞에 (누적) 글자 자르기
         embed = discord.Embed(title="국내 코로나 확진자 수 현황", description="http://ncov.mohw.go.kr/ 의 정보를 가져옵니다.",
                               color=0x005666)
         embed.add_field(name="일일 확진자",
-                        value=f"총: {today_domestic + today_overseas}, 국내: {today_domestic}, 해외유입: {today_overseas}",
+                        value=f"총: {today_total}명, 국내: {today_domestic}명, 해외유입: {today_overseas}명",
                         inline=False)
         embed.add_field(name="누적 확진자", value=f"{accumulate_confirmed}명", inline=False)
         await ctx.send(embed=embed)
